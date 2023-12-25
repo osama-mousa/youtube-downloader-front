95a7a322af8e3b97a16855cfb59d508819a6bfd9
osamasayd <osamasayd141@gmail.com>
2023-12-26 00:08:35 +0300

    add Navbar img and ps text

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import './Navbar.css';
+import reactLogo from '../assets/Hacker.png';
 
 const Navbar = () => {
     return (
-        <AppBar position="fixed" className='nav' sx={{backgroundColor:'#000'}}>
+        <AppBar className='nav' sx={{ backgroundColor: '#000' }}>
             <Toolbar>
+                <img src={reactLogo} alt="React Logo" style={{ width: '40px', marginRight: '10px' }} />
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Youtube Downloader
                 </Typography>
             </Toolbar>
+            <p className='PsStand'>I stand with Palestine 🇵🇸 </p>
         </AppBar>
     );
 };
