ccaad3421bf490069df7b973fd37410f68e431af
osamasayd <osamasayd141@gmail.com>
2023-12-27 11:54:55 +0300

    edit footer style

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className='footer'>
-            <Typography variant="body1" color="inherit">
+            <Typography variant="body1" color="inherit" font-size="small">
                 © 2024 <a href="https://www.linkedin.com/in/osama-mousa-%F0%9F%87%B5%F0%9F%87%B8-568364209/" target="_blank">OSS</a> All rights reserved.
             </Typography>
         </div>
