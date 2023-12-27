9bb92504e987b4a0d27fe816e0ba9a470a6ed726
osamasayd <osamasayd141@gmail.com>
2023-12-27 10:13:59 +0300

    adding a to footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className='footer'>
             <Typography variant="body1" color="inherit">
-                © 2024 OSS All rights reserved.
+                © 2024 <a href="https://www.linkedin.com/in/osama-mousa-%F0%9F%87%B5%F0%9F%87%B8-568364209/" target="_blank">OSS</a> All rights reserved.
             </Typography>
         </div>
     );
